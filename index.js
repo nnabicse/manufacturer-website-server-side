2044264b9d8cb30fb9d73d564529cb9df50731c8
Nurun Nabi <nnabi.cse@gmail.com>
2022-05-28 20:52:59 +0600

    add product issue sove

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ async function run() {
         }
 
 
-        app.get('/admin/:email', verifyJWT, async (req, res) => {
+        app.get('/admin/:email', async (req, res) => {
             const email = req.params.email;
             const user = await usersCollection.findOne({ email: email });
             const isAdmin = user.role === 'admin';
